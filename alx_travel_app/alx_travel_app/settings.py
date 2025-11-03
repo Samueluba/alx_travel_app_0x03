2dc51e92939b77ee9656d7ce74e77f63d68e6aab
Samueluba <111093750+Samueluba@users.noreply.github.com>
2025-11-03 11:17:16 +0100

    Update settings.py

--- alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/settings.py
@@ -1,1 +1,9 @@
+# alx_travel_app/settings.py
+
+# Celery Configuration
+CELERY_BROKER_URL = 'amqp://localhost'  # RabbitMQ URL
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_BACKEND = 'rpc://'
+CELERY_TIMEZONE = 'UTC'
 
