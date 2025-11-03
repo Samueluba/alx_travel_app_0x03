93d653e69b3cd4bf29d64e0983de4a924f7bb8de
Samueluba <111093750+Samueluba@users.noreply.github.com>
2025-11-03 11:16:36 +0100

    Update views.py

--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -1,1 +1,21 @@
+# listings/views.py
+
+from rest_framework import viewsets
+from .models import Booking
+from .serializers import BookingSerializer
+from .tasks import send_booking_confirmation_email
+from rest_framework.response import Response
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+    def perform_create(self, serializer):
+        # Save the booking instance
+        booking = serializer.save()
+
+        # Trigger the email task asynchronously using delay
+        send_booking_confirmation_email.delay(booking.id, booking.user.email)
+
+        return Response(serializer.data)
 
